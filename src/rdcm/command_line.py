295370de535ba1904ed0a25e3b531747cdeff0dd
Andre Pearce <andrepearce22@gmail.com>
2019-08-12 12:03:40 +1000

    Re-structure python package

--- src/rdcm/command_line.py
+++ src/rdcm/command_line.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
+import rdcm
 
 def main(argv=None):
     print(argv)
     print("hi")
+    rdcm.test()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rdcm',
+      version='0.1',
+      description='Route53 domain certificate manager',
+      url='https://github.com/andrepearce/rdcm/tree/dev/rdcm',
+      author='Andre Pearce',
+      author_email='andrepearce22@gmail.com',
+      license='MIT',
+      packages=['rdcm'],
+      zip_safe=False)
